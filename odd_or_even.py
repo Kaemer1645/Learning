9454b003ec10c6de99e3f1aec21b2c93771cf376
Kaemer1645 <szymex23@o2.pl>
2020-09-05 22:41:20 +0200

    update

---
+++ odd_or_even.py
@@ -0,0 +1,9 @@
+a = input('Please input a number: ')
+if  int(a) % 2 == 0:
+    print('This number is even')
+else:
+    print('This number is oven')
+
+if int(a) % 4 == 0:
+    print('This number is a multiple of 4')
+
