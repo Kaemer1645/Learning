286484755381bc104afc6a2e176044bd032d71f9
Kaemer1645 <szymex23@o2.pl>
2020-09-11 13:30:53 +0200

    update

--- PracticePython/List_end.py
+++
@@ -1,8 +0,0 @@
-a = [5, 10, 15, 20, 25]
-
-def first_last(list):
-    return print('First element of list is: '+str(list[0])+' Last element of list is: '+str(list[-1]))
-
-
-
-solution = first_last(a)
--- PracticePython/odd_or_even.py
+++
@@ -1,9 +0,0 @@
-a = input('Please input a number: ')
-if  int(a) % 2 == 0:
-    print('This number is even')
-else:
-    print('This number is oven')
-
-if int(a) % 4 == 0:
-    print('This number is a multiple of 4')
-
